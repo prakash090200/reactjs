e56bbb2f1dd398865545445eabf630b5b6c6c7bb
prakash <prakash.kumar2018a@vitstudent.ac.in>
2022-01-03 20:59:06 +0530

    first commit

--- src/Components/CompB.js
+++ src/Components/CompB.js
@@ -11,7 +11,7 @@ export default function CompB() {
     return (
         <div>
             <b>Hey , Bro this is {firstname}    {lastname}       (Using UseContext HOOK)</b>
-            <h6 style={{color:appcolor}}>This is green color</h6>
+            <h6 style={{color:appcolor}}>  This is green color yes</h6>
             -------------------------------
              <CompC/>
         </div>
