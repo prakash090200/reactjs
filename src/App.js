e03631598c297866ba7bf958160e1531b3f9dfb3
prakash <prakash.kumar2018a@vitstudent.ac.in>
2022-01-03 21:55:52 +0530

    f s

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import './App.css';
 import React,{createContext,useState} from 'react'
 import CompA from './Components/CompA'
 
+
 const fname=createContext();
 const lname=createContext();
 const colgroup=createContext();
 function App() {
-
+  
   const [col,setCol]=useState("green");
+
   return (
     <div className="App">
     <fname.Provider value={"Prakash"}>
     <lname.Provider value={"Kumar"}>
-    <colgroup.Provider value={{appcolor:col}}>
-
+    <colgroup.Provider value={{appcolor:col}}>  
+      
      <CompA/>
 
      </colgroup.Provider>
